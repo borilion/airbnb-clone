6ad8a4c2dab5d86b52e2b91242aa392d032457f4
borilion <kyoungseok.heo@gmail.com>
2022-07-25 17:42:29 +0900

    20220725-2

--- rooms/admin.py
+++ rooms/admin.py
@@ -2,6 +2,21 @@ from django.contrib import admin
 from . import models
 
 
+@admin.register(models.RoomType)
+class RoomTypeAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.Amenity)
+class AmenityAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.HouseRule)
+class HouseRuleAdmin(admin.ModelAdmin):
+    pass
+
+
 # Register your models here.
 @admin.register(models.Room)
 class RoomAdmin(admin.ModelAdmin):
--- rooms/models.py
+++ rooms/models.py
@@ -4,6 +4,40 @@ from django_countries.fields import CountryField
 from users import models as user_models
 
 
+class AbstractItem(core_models.TimeStampedModel):
+
+    """Abstract Item"""
+
+    name = models.CharField(max_length=80)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.name
+
+
+class RoomType(AbstractItem):
+
+    """Room Type Object Definition"""
+
+    pass
+
+
+class Amenity(AbstractItem):
+
+    """Amenity Object Definition"""
+
+    pass
+
+
+class HouseRule(AbstractItem):
+
+    """House Rule Object Definition"""
+
+    pass
+
+
 # Create your models here.
 class Room(core_models.TimeStampedModel):
 
@@ -23,6 +57,9 @@ class Room(core_models.TimeStampedModel):
     check_out = models.TimeField(null=True, blank=True)
     instant_book = models.BooleanField(default=False)
     host = models.ForeignKey(user_models.User, on_delete=models.CASCADE, null=True)
+    room_type = models.ManyToManyField(RoomType, blank=True)
+    amenities = models.ManyToManyField(Amenity, blank=True)
+    house_rules = models.ManyToManyField(HouseRule, blank=True)
 
     def __str__(self):
         return self.name
